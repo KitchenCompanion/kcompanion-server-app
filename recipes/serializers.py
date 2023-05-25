2c2ec38f1e822d387144ab3854d2213fc72e7bf2
kimbifavour <kimbifavour@gmail.com>
2023-05-25 04:51:55 +0100

    feat: created the signup user authentication API

--- recipes/serializers.py
+++ recipes/serializers.py
@@ -1,5 +1,31 @@
 from rest_framework import serializers
+from django.contrib.auth import get_user_model
 from .models import *
+
+User = get_user_model()
+
+class UserCreateSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
+    confirm_password = serializers.CharField(write_only=True)
+    
+    class Meta:
+        model = User
+        fields = ['email', 'username', 'password', 'confirm_password']
+
+    def validate(self, data):
+        if data['password'] != data['confirm_password']:
+            raise serializers.ValidationError("Passwords do not match")
+        return data
+
+    def create(self, validated_data):
+        user = User.objects.create(
+            email=validated_data['email'],
+            username=validated_data['username']
+        )
+        user.set_password(validated_data['password'])
+        user.save()
+        return user
+
 
 class PersonSerializer(serializers.ModelSerializer):
     class Meta:
--- recipes/urls.py
+++ recipes/urls.py
@@ -2,10 +2,15 @@ from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 from . import views
 
+from .views import UserCreateView
+
 router = DefaultRouter()
 
 router.register('person', views.PersonView)
-router.register('recipe', views.RecipeView)
 router.register('review', views.ReviewView)
 
-urlpatterns =  router.urls
+urlpatterns =  [
+    path('recipe/', views.recipe_list),
+    path('recipe/<int:id>', views.recipe_detail, name='recipe'),
+    path('signup/', UserCreateView.as_view(), name='signup'),
+] + router.urls
